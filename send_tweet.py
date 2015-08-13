909a898f528151a56833aee29bd9bab0a5b3301f
Jeel Patel <jeelpatel007@yahoo.com>
2015-08-12 21:32:30 -0400

    Create send_tweet.py

---
+++ send_tweet.py
@@ -0,0 +1,26 @@
+# Tweepy is used to access the Twitter API
+import tweepy
+
+# **get_api** takes the auth values and pushes it to twitter API using Tweepy
+def get_api(cfg):
+  auth = tweepy.OAuthHandler(cfg['consumer_key'], cfg['consumer_secret'])
+  auth.set_access_token(cfg['access_token'], cfg['access_token_secret'])
+  return tweepy.API(auth)
+
+# **main** gets the data to be used in *get_api* 
+def main():
+  # Fill in the values noted in previous step here
+  cfg = { 
+    "consumer_key"        : "VALUE_HERE",
+    "consumer_secret"     : "VALUE_HERE",
+    "access_token"        : "VALUE_HERE",
+    "access_token_secret" : "VALUE_HERE" 
+    }
+
+  api = get_api(cfg)
+  tweet = raw_input("Enter your Tweet: \t")
+  status = api.update_status(status=tweet) 
+  # Yes, tweet is called 'status' rather confusing
+
+if __name__ == "__main__":
+  main()
